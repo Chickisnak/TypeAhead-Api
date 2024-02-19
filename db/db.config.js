cb7535260fb4fbe08cf1b295debf8a9657f8968a
Akhand Pratap Singh <104713627+Chickisnak@users.noreply.github.com>
2024-02-19 22:28:41 +0530

    Update db.config.js

--- db/db.config.js
+++ db/db.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async ()=>{
   try {
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect("mongodb+srv://chickisnak:bslf3IjnlnBmyGD8@cluster0.cnkleiy.mongodb.net/?retryWrites=true&w=majority");
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
