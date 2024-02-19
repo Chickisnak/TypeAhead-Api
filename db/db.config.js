3bc9f4f8900199f236f4f740fe35b8a246166cf5
Akhand Pratap Singh <104713627+Chickisnak@users.noreply.github.com>
2024-02-19 22:20:55 +0530

    Update db.config.js

--- db/db.config.js
+++ db/db.config.js
@@ -5,8 +5,6 @@ const connect = async ()=>{
     await mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
     });
     console.log('Connected to MongoDB');
   } catch (error) {
