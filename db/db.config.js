539e03813e97a3bc2c2cb31d74e4703010add315
Akhand Pratap Singh <104713627+Chickisnak@users.noreply.github.com>
2024-02-19 22:17:52 +0530

    Update db.config.js

--- db/db.config.js
+++ db/db.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async ()=>{
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
